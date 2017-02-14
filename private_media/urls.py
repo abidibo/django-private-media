5f8b1e8e90184dc40062417a43c89fd83f894a5a
abidibo <abidibo@gmail.com>
2017-02-14 12:40:42 +0100

    fixes 'cannot import name patterns' in urls

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf import settings
 
-urlpatterns = patterns('private_media.views',
+urlpatterns = [
     url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), 'serve_private_file',),
-)
+]
